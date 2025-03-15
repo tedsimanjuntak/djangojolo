7f39c21a69af8c0cf72fac62153878f4a2964e95
Teddy Simanjuntak <tedsimanjuntak@gmail.com>
2025-03-15 19:36:29 +0100

    Rem: dummy data as now we have real data stored in sql database

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 14, 2025'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 15, 2025'
-    }
-]
 
 # note: render stil returns an HttpResponse instance (or an exception).
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
