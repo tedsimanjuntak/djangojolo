d6b246f05e6a0c25f1c453c52f745aab472362c5
Teddy Simanjuntak <tedsimanjuntak@gmail.com>
2025-03-15 20:58:09 +0100

    first do pip install django-crispy-forms then add crispy_forms in INSTALLED_APPS and at the end tell crispy_forms which bootstrap to use. I chose 'bootstrap5'

--- djangojolo/settings.py
+++ djangojolo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "crispy_forms",
     "users.apps.UsersConfig",
     "blog.apps.BlogConfig",
     "django.contrib.admin",
@@ -123,3 +124,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
