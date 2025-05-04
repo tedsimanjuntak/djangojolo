3a25661998954f8b7a9a3bff4b1fc99eb41c93a8
Teddy Simanjuntak <tedsimanjuntak@gmail.com>
2025-05-04 17:05:34 +0200

    Add: login and logout template

--- djangojolo/urls.py
+++ djangojolo/urls.py
@@ -24,10 +24,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', user_views.register, name='register'),
 ]
 
 
-
+# 
