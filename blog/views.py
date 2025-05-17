ae1780b06075e579279265d145600520f820eb23
Teddy Simanjuntak <tedsimanjuntak@gmail.com>
2025-05-17 21:29:38 +0200

    Mod: by setting these 3 attributes should do it

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,8 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
+    template_name = 'blog/home.html'    # <app>/<model>_<viewtype>.html 
+    context_object_name = 'posts'
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
